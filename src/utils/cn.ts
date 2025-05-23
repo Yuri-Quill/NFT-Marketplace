4fe9bf62bb416acaa348fbb92964eff2aa641f4e
Yuri-Quill <yuri.i.tdevs@gmail.com>
2025-05-23 05:48:19 +0300

    feat: implement utility function for conditional class names using clsx

---
+++ src/utils/cn.ts
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+
+interface ICnProps {
+	baseClass: string;
+	modifier?: string;
+	mod?: boolean;
+	additionalClass?: string;
+}
+
+const cn = ({ baseClass, modifier, mod = false, additionalClass }: ICnProps): string => {
+	const className = clsx(baseClass, mod && modifier ? `${baseClass}--${modifier}` : null, additionalClass);
+	return className;
+};
+
+export default cn;
