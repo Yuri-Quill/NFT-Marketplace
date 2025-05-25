1a18fbbd528a3da05e1d945289bae27bff83f522
Yuri-Quill <yuri.i.tdevs@gmail.com>
2025-05-25 06:24:11 +0300

    feat: update tsconfig to disable erasableSyntaxOnly and add routes constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,12 @@
+export enum AuthRoutes {
+	Login = "/auth/login",
+	Register = "/auth/register",
+}
+
+export enum NavRoutes {
+	AboutUs = "/about-us",
+	Marketplace = "/marketplace",
+	Contacts = "/contacts",
+	Rankings = "/rankings",
+	Profile = "/profile",
+}
