8f910619addc3e08b5e5afc8b7001ae7b91b6f4b
Yuri-Quill <yuri.i.tdevs@gmail.com>
2025-05-25 03:59:19 +0300

    feat: implement Input component with SCSS styling and Formik integration

---
+++ src/UI/Input/Input.tsx
@@ -0,0 +1,30 @@
+import { ErrorMessage, Field } from "formik";
+import type { JSX } from "react";
+
+import cn from "@/utils/cn";
+
+import "./Input.scss";
+
+type inputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+	name: string;
+	children?: React.ReactNode;
+	icon?: React.ReactElement;
+	additionalIcon?: React.ReactElement;
+	as?: keyof JSX.IntrinsicElements | React.ComponentType<object>;
+};
+
+const Input = ({ name, className, type, children, icon, additionalIcon, as, ...rest }: inputProps) => {
+	return (
+		<div className={cn({ baseClass: "input", additionalClass: className })}>
+			{icon && icon}
+
+			<Field className="input__field" name={name} type={type} children={children} as={as} {...rest} />
+
+			{additionalIcon && additionalIcon}
+
+			<ErrorMessage className="input__error" name={name} component="span" />
+		</div>
+	);
+};
+
+export default Input;
