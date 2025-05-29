ec2a4dc3fa2176b1643871e3f87ba3d1337135ef
Yuri-Quill <yuri.i.tdevs@gmail.com>
2025-05-29 23:41:03 +0300

    feat: integrate ToastContainer into Layout and clean up main imports

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,14 +1,27 @@
 import { Suspense } from "react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 const Layout = () => {
 	return (
 		<>
 			<ScrollRestoration />
+			<ToastContainer
+				position="top-right"
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={true}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="colored"
+			/>
 
 			<header>Header</header>
 
-			<main>
+			<main className="main" role="main">
 				<Suspense fallback="Loading">
 					<Outlet />
 				</Suspense>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "@/styles/reset.scss";
-import "react-toastify/dist/ReactToastify.css";
-import "./index.scss";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+
+import "@/styles/reset.scss";
+
+import "@assets/fonts/Work_Sans/fonts.scss";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
